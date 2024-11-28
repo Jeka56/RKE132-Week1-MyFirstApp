2822d553346158c4ac771fb6cb5557d500edb53f
Jeka56 <jekaterina.tsumakova@gmail.com>
2024-11-28 20:22:24 +0200

    create new app

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// This is a comment to my code
+Console.WriteLine("Enter your name:");
+//string - sõne
+string userName;
+userName = Console.ReadLine();
+Console.WriteLine("Hello" +  userName);
+
+Console.WriteLine($"Hello, {userName} !");
+
